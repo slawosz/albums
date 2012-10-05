3ae6cd1178481e596db166ce424276589235b122
Sławosz <slawosz@gmail.com>
2012-10-05 14:40:42 +0200

    rails new albums

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Albums::Application.config.secret_token = '95d3a4559530b7c596d94339c229fbd2c7d6a50c69dc66ade84b9329198cd92033e00caea662716b6752431b86cd0901acdd69c7b304f24df5f58b67937145f6'
